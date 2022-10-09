9b14a6f75c79c1382faed149603b2794e0fdf1ef
Evgeny <shadrin_pharm@mail.ru>
2022-10-09 15:35:02 +0300

    Решили вторую задачу

---
+++ task_50/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int[,] array = new int[3, 4];
+Random rand = new Random();
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = rand.Next(1, 11);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+
+void searchElement(int i, int j)
+{
+    if (i > array.GetLength(0) || j > array.GetLength(1))
+    {
+        Console.WriteLine("Такого числа в массиве нет");
+    }
+    else
+    {
+        Console.WriteLine(array[i, j]);
+    }
+}
+
+// searchElement(2,2);
